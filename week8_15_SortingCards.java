2275703b5db665e633d2c7d2959d3cbd853d09c9
CCekla <everinblack@hotmail.it>
2018-03-11 22:48:41 +0100

    Update and rename Card.java to week8_15_SortingCards.java

--- week8_15_SortingCards.java
+++ week8_15_SortingCards.java
@@ -45,3 +45,67 @@ public class Card implements Comparable<Card> {
     }
 
 }
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Hand implements Comparable<Hand> {
+    private List<Card> cards;
+    
+    public Hand(){
+        this.cards = new ArrayList<Card>();
+    }
+    
+    public void add(Card card){
+        this.cards.add(card);
+    }
+    
+    public void print(){
+        for(Card card : this.cards){
+            System.out.println(card);
+        }
+    }
+    
+    public void sort(){
+        Collections.sort(this.cards);
+    }
+    
+    public int Values(){
+        int sum = 0;
+        for(Card card : this.cards){
+            sum += card.getValue();
+        }
+        return sum;
+    }
+    
+    public int Suits(){
+        int sum = 0;
+        for(Card card : this.cards){
+            sum += card.getSuit();
+        }
+        return sum;
+    }
+    
+    @Override
+    public int compareTo(Hand hand){
+        return this.Values() - hand.Values();
+    }
+    
+    public void sortAgainstSuit(){
+        Collections.sort(this.cards, new SortAgainstSuitAndValue());
+    }
+}
+
+
+import java.util.Comparator;
+
+public class SortAgainstSuitAndValue implements Comparator<Card>{
+    public int compare(Card card, Card myCard){
+        if(card.getSuit() - myCard.getSuit() == 0){
+            return card.getValue() - myCard.getValue();
+        }
+        return card.getSuit() - myCard.getSuit();
+    }
+}
