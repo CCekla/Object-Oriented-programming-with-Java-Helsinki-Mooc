7fdc37a3a876a2b3ac2d8b4391e53ef4d8e76be6
CCekla <everinblack@hotmail.it>
2018-03-19 15:46:50 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+
+package people;
+
+public class Person {
+    private String name;
+    private String adress;
+    
+    public Person(String name, String adress){
+        this.name = name;
+        this.adress = adress;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + "\n" +
+        "  " + this.adress;
+    }
+}
